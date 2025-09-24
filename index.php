81914d737e8d2d4a891fa048b5700d900ed72451
sofia <soofiimaya@gmail.com>
2025-09-24 09:27:01 +0100

    Carga Inicial

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Lanzarote</title>
+  <link rel="stylesheet" href="styles.css" content="width=device-width,initial-scale=1-0">
+</head>
+<body>
+  <? echo "hola mundo" ?>
+</body>
+</html>
+
+
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDKH3YzYbLkKSb3QN8efZmFsudHHhXm3XnMr+d71Ch+bl3MQ1R4+fBmPecjMDhYofuLy+ImX5HbSzr3GtcFbqW8qbmwg1p1n4EGFOfhLlYPpQo/daDoSY0498mIDxKAfZiscETr9IAzmV7DpbWtXBCDJ/FXwlHQoDlkZQNCidTlPIax//j93q4D2Zqik2n2KzeBPKY6s3S4bNn1wyGNuFJo32fEO3e68bSlDdg7HYXZcjobQKB2NIbRHy7VfU1ybEtxLcB8F4Bjc9FKgEk5TCLijjM+Zs2LCBw09ZtehOuPo5GS23MYjgySoy1MXrymUnDTGc2KDfgEpq3ekTCnsFb5z4SoB07ymqalgHayO2eVn77A6vFSoZWpvai0XqjJxLTu8r9w+pYwNvpFnwQ7WOrohyZ5qmAL0m8Tja82OlD0dE2JXMVL3j6bLTJ2kRIMi4boxsz8HriIMDAuu79l7Fwl9V9vl5nWVBMw4sgOMXj9u9tm9jJzbKtgecF5ojZ9u1k= sofia@E16-05
